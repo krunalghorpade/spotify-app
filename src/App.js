724db7285224e7173a6680eecd18a5fe1dae0123
Krunal <krunalghorpade365@gmail.com>
2018-02-21 21:48:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+let priCol = "green"
+let secCol = "#4d4d4d"
+
+
+class Playlist extends Component{
+  render() {
+    return (
+      <div className="soundcard">
+        <img/>
+        <h3>Playlist</h3>
+        <ul className="sc-list">
+          <li>song</li>
+          <li>song</li>
+          <li>song</li>
+          <li>song</li>
+        </ul>
+
+      </div>
+    );
+  }
+}
+
+
+class Aggregate extends Component{
+  render() {
+    return (
+      <div style={{'width' : '40%', 'display' : 'inline-block'}}>
+        <h3 style={{'color': 'green'}}>Number Text
+        </h3>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component{
+  render() {
+    return (
+      <div className="filter">
+        <img/>
+        <input type="text"/>
+        <span style={{"color": secCol}}>Filter</span>
+
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+  
+    return (
+      <div className="App">
+        <h1>Title</h1>
+        <Aggregate/>
+        <Aggregate/>
+        <Filter/>
+        <Playlist/>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
